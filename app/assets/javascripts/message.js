c78b9af3add3e4a689c2b277f87018273eed0160
fermentedsoybean7110 <62579566+fermentedsoybean7110@users.noreply.github.com>
2020-04-01 21:17:11 +0900

    4/1_end

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,61 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image) {
+      var html =`
+        <div class="main__contents__text">
+          <div class="main__contents__text__subjects">
+            <div class="main__contents__text__subjects__title">
+              ${message.user_name}
+            </div>
+            <div class="main__contents__text__subjects__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="main__contents__text__sentence">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} class="lower-message__image">
+        </div>`
+    } else {
+      var html =`
+      <div class="main__contents__text">
+          <div class="main__contents__text__subjects">
+            <div class="main__contents__text__subjects__title">
+              ${message.user_name}
+            </div>
+            <div class="main__contents__text__subjects__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="main__contents__text__sentence">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+      </div>`
+    };
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url, 
+      type: "POST",
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main__contents').append(html);
+    })
+    .fail(function(){
+      console.log("nnnn");
+    });
+  })
+});
